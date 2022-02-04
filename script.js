c1b17b5f8b00080f1aef72c27cb941828c662eaa
Marek220v <98671729+Marek220v@users.noreply.github.com>
2022-02-04 15:25:59 +0100

    newest04/02/2022

---
+++ script.js
@@ -0,0 +1,69 @@
+const box1 = document.querySelector(".box1")
+const box2 = document.querySelector(".box2")
+const box3 = document.querySelector(".box3")
+const box4 = document.querySelector(".box4")
+const box5 = document.querySelector(".box5")
+const box6 = document.querySelector(".box6")
+const box8 = document.querySelector(".box8")
+const box9 = document.querySelector(".box9")
+const box10 = document.querySelector(".box10")
+const box11 = document.querySelector(".box11")
+const box12 = document.querySelector(".box12")
+
+let zmienna1 = document.querySelector(".box1")
+let zmienna2 = document.querySelector(".box2")
+let zmienna3 = document.querySelector(".box3")
+let zmienna4 = document.querySelector(".box4")
+let zmienna5 = document.querySelector(".box5")
+let zmienna6 = document.querySelector(".box6")
+let zmienna7 = document.querySelector(".box8")
+let zmienna8 = document.querySelector(".box9")
+let zmienna9 = document.querySelector(".box10")
+let zmienna10 = document.querySelector(".box11")
+let zmienna11 = document.querySelector(".box12")
+
+zmienna1.addEventListener("click" , () => {
+
+    zmienna1.classList.toggle("fullscreen")
+       
+})
+zmienna2.addEventListener("click" , () => {
+
+    zmienna2.classList.toggle("fullscreen");    
+})
+zmienna3.addEventListener("click" , () => {
+
+    zmienna3.classList.toggle("fullscreen");    
+})
+zmienna4.addEventListener("click" , () => {
+
+    zmienna4.classList.toggle("fullscreen");    
+})
+zmienna5.addEventListener("click" , () => {
+
+    zmienna5.classList.toggle("fullscreen");    
+})
+zmienna6.addEventListener("click" , () => {
+
+    zmienna6.classList.toggle("fullscreen");    
+})
+zmienna7.addEventListener("click" , () => {
+
+    zmienna7.classList.toggle("fullscreen1");    
+})
+zmienna8.addEventListener("click" , () => {
+
+    zmienna8.classList.toggle("fullscreen1");    
+})
+zmienna9.addEventListener("click" , () => {
+
+    zmienna9.classList.toggle("fullscreen1");    
+})
+zmienna10.addEventListener("click" , () => {
+
+    zmienna10.classList.toggle("fullscreen1");    
+})
+zmienna11.addEventListener("click" , () => {
+
+    zmienna11.classList.toggle("fullscreen1");    
+})
